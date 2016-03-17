d090dd6c2be5cd753d6abe79ad9159fa94a99da5
GIV <giyyanan@andrew.cmu.edu>
2016-03-17 04:17:02 -0400

    v 1.0

---
+++ JS/index.js
@@ -0,0 +1,7 @@
+var  initialize = function ()
+{
+	document.getElementById("home").style.height = (window.innerHeight)+"px";
+	document.getElementById("about").style.height = (window.innerHeight)+"px";
+	document.getElementById("initiatives").style.height = (window.innerHeight)+"px";
+	document.getElementById("help").style.height = (window.innerHeight)+"px";
+}
