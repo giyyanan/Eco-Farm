00ef1f11abd11230d3fea0d5c23d7414da34bce7
GIV <giyyanan@andrew.cmu.edu>
2016-03-19 23:16:28 -0400

    small screens working now...removed the unnecessary options for unavailable content

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,4 @@
 var  initialize = function ()
 {
-	document.getElementById("home").style.height = (window.innerHeight)+"px";
-	document.getElementById("about").style.height = (window.innerHeight)+"px";
-	document.getElementById("initiatives").style.height = (window.innerHeight)+"px";
-	document.getElementById("help").style.height = (window.innerHeight)+"px";
+	
 }
