08580ced1af07c193e5dab1f20cdb06a66eb1410
GIV <giyyanan@andrew.cmu.edu>
2016-04-17 08:57:57 -0400

    team and contact

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,8 @@
 var  initialize = function ()
 {
-	
+	var mapCanvas = document.getElementById("contact_map");
+	var mapOptions = {
+		center: new google.maps.LatLng(11.02,76.96), zoom: 10
+	}
+	var map = new google.maps.Map(mapCanvas, mapOptions);
 }
